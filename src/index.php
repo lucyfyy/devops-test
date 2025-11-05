a4fece33e2acc8941a86db216966d92858ce52dc
Bambang Dwi <89371703+lucyfyy@users.noreply.github.com>
2025-11-05 10:30:52 +0700

    Create index.php with basic HTML structure

---
+++ src/index.php
@@ -0,0 +1,13 @@
+<?php
+// index.php
+?>
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Hello</title>
+</head>
+<body>
+  <h1><?php echo "Hallo Dirubah"; ?></h1>
+</body>
+</html>
